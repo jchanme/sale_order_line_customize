f3107f11b0a67710244609adce69dab8c4bef332
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-30 14:34:55 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Order Line Customize",
+    'summary': """
+        Adds custom fields to Sale Order Lines""",
+    'description': """
+        This Module Adds Custom Fields to order Lines in Sales orders and in Purchase orders Dynamically After the Products added and Saved.
+    """,
+    'author': "Safyric Co., Ltd.",
+    'website': "https://www.safyric.com",
+    'category': 'Business',
+    'version': '0.5',
+    'depends': ['base', 'sale', 'purchase'],
+    'images': [
+        'images/main.jpg',
+    ],
+    'data': [
+        'views/sale_order_line_template.xml',
+        'views/purchase_order_line_template.xml',
+    ],
+}
